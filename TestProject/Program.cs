9bfcef92ea35272510f003febb615cce59d3771c
Noble Chad <gameru2003@gmail.com>
2023-06-20 14:24:15 +0300

    Added rediredct to a NotFound.cshtml when page is not found(404)

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 using TestProject.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -27,17 +28,13 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseMiddleware<NotFoundRedirectMiddleware>();
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
-
 app.Run();
